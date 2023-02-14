9047b42e2c94064f2afb15a41fe9f2bd6804e54f
Shivam Govind Rao <shivamgrao9161@gmail.com>
2023-02-14 14:36:51 +0530

    category style

--- src/components/CATEGORY/Category.jsx
+++ src/components/CATEGORY/Category.jsx
@@ -13,38 +13,39 @@ const Category = () => {
                 <div className="product">                 <div className="img">
                     <img src="https://media.istockphoto.com/id/467970412/vector/groceries.jpg?b=1&s=612x612&w=0&k=20&c=9FmWWVDwlMNMVhxm76fYV1wxU-U2i2kmURhu7UXGzmc=" alt="" />
                 </div>
-                    <p> GROCERY </p>
+                    <p className='p-tittle'> GROCERY </p>
                 </div>
+
                 <div className="product">                 <div className="img">
                     <img src="https://cdn.shopify.com/s/files/1/0245/9382/5867/products/apple-mobile-phones-apple-iphone-12-pro-128gb-imported-1-year-international-warranty-16090426212427_800x.jpg?v=1647757688" alt="" />
                 </div>
-                    <p> MOBILES </p>
+                    <p className='p-tittle'> MOBILES </p>
 
                 </div>
                 <div className="product">                 <div className="img">
                     <img src="https://thumbs.dreamstime.com/b/sexy-men-holding-hand-woman-s-thigh-pose-studio-sexy-man-holding-hand-woman-s-thigh-110266614.jpg" alt="" />
                 </div>
-                    <p> FASHION </p>
+                    <p className='p-tittle'> FASHION </p>
 
                 </div>
                 <div className="product">
                     <div className="img">
                         <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRevIXWXoy3ga-QMwrink0LQVD4R_xOTgMqTg&usqp=CAU" alt="" />
                     </div>
-                    <p> ELECTRONICS </p>
+                    <p className='p-tittle'> ELECTRONICS </p>
 
                 </div>
                 <div className="product">              <div className="img">
                     <img src="https://img.freepik.com/premium-vector/household-appliances-group-household-appliances-white-background_39422-20.jpg" alt="" />
                 </div>
-                    <p> APPLIANCES </p>
+                    <p className='p-tittle'> APPLIANCES </p>
 
                 </div>
 
                 <div className="product">                    <div className="img">
                     <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSyFZK3X9WCEcEmtkggMtALhFoqZDIKSF8c1Q&usqp=CAU" alt="" />
                 </div>
-                    <p> TOP DEALS</p>
+                    <p className='p-tittle'> TOP DEALS</p>
 
                 </div>
 
